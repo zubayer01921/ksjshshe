fd468d45b1ff2d0b69ce889f1848153d913aa22e
zubayer01921 <92502844+zubayer01921@users.noreply.github.com>
2021-11-21 13:16:46 +0600

    Create user.py

---
+++ user.py
@@ -0,0 +1,23 @@
+#@title **Create User**
+#@markdown Enter Username and Password
+
+import os
+
+username = "user" 
+password = "a" 
+
+print("Creating User and Setting it up")
+
+# Creation of user
+os.system(f"useradd -m {username}")
+
+# Add user to sudo group
+os.system(f"adduser {username} sudo")
+    
+# Set password of user to 'root'
+os.system(f"echo '{username}:{password}' | sudo chpasswd")
+
+# Change default shell from sh to bash
+os.system("sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd")
+
+print("User Created and Configured")
