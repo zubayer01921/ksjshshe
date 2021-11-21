b377e0161be6fa5d65cf6cf2717a1ca861db8dfb
zubayer01921 <92502844+zubayer01921@users.noreply.github.com>
2021-11-21 13:19:18 +0600

    Create cnn.py

---
+++ cnn.py
@@ -0,0 +1,67 @@
+#@title **RDP**
+#@markdown  It takes 4-5 minutes for installation
+
+import os
+import subprocess
+
+#@markdown  Visit http://remotedesktop.google.com/headless and Copy the command after authentication
+
+CRP = "DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AX4XfWhOcjwtyPjXdKwNxp0G0KVMLp2G3LMfH1ml7pBN4Oiol3BCWN7scbv1Z5_iluEPGw" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)" 
+
+#@markdown Enter a pin more or equal to 6 digits
+Pin = 123456 
+
+
+class CRD:
+    def __init__(self):
+        os.system("apt update")
+        self.installCRD()
+        self.installDesktopEnvironment()
+        self.installGoogleChorme()
+        self.finish()
+
+    @staticmethod
+    def installCRD():
+        print("Installing Chrome Remote Desktop")
+        subprocess.run(['wget', 'https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb'], stdout=subprocess.PIPE)
+        subprocess.run(['dpkg', '--install', 'chrome-remote-desktop_current_amd64.deb'], stdout=subprocess.PIPE)
+        subprocess.run(['apt', 'install', '--assume-yes', '--fix-broken'], stdout=subprocess.PIPE)
+
+    @staticmethod
+    def installDesktopEnvironment():
+        print("Installing Desktop Environment")
+        os.system("export DEBIAN_FRONTEND=noninteractive")
+        os.system("apt install --assume-yes xfce4 desktop-base xfce4-terminal")
+        os.system("bash -c 'echo \"exec /etc/X11/Xsession /usr/bin/xfce4-session\" > /etc/chrome-remote-desktop-session'")
+        os.system("apt remove --assume-yes gnome-terminal")
+        os.system("apt install --assume-yes xscreensaver")
+        os.system("systemctl disable lightdm.service")
+
+    @staticmethod
+    def installGoogleChorme():
+        print("Installing Google Chrome")
+        subprocess.run(["wget", "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"], stdout=subprocess.PIPE)
+        subprocess.run(["dpkg", "--install", "google-chrome-stable_current_amd64.deb"], stdout=subprocess.PIPE)
+        subprocess.run(['apt', 'install', '--assume-yes', '--fix-broken'], stdout=subprocess.PIPE)
+
+    @staticmethod
+    def finish():
+        print("Finalizing")
+        os.system(f"adduser {username} chrome-remote-desktop")
+        command = f"{CRP} --pin={Pin}"
+        os.system(f"su - {username} -c '{command}'")
+        os.system("service chrome-remote-desktop start")
+        print("Finished Succesfully")
+
+
+try:
+    if username:
+        if CRP == "":
+            print("Please enter authcode from the given link")
+        elif len(str(Pin)) < 6:
+            print("Enter a pin more or equal to 6 digits")
+        else:
+            CRD()
+except NameError as e:
+    print("username variable not found")
+    print("Create a User First")
